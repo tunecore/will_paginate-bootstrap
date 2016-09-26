3cabf759ceb782a7d0a792b610636375e7924fb5
Amit Gulati <agulati@tunecore.com>
2016-09-26 10:13:41 -0400

    modify css to support bootstrap v4 alpha

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -15,7 +15,7 @@ module BootstrapPagination
         end
       end.join(@options[:link_separator])
 
-      tag("ul", list_items, class: ul_class)
+      tag("nav", tag("ul", list_items, class: ul_class), "aria-label" => @options[:label], class: @options[:class])
     end
 
     def container_attributes
@@ -28,38 +28,38 @@ module BootstrapPagination
       link_options = @options[:link_options] || {}
 
       if page == current_page
-        tag("li", tag("span", page), class: "active")
+        tag("li", tag("span", "#{page} #{tag("span", "(current)", class: "sr-only")}", class: "page-link"), class: "page-item active")
       else
-        tag("li", link(page, page, link_options.merge(rel: rel_value(page))))
+        tag("li", link(page, page, link_options.merge(class: "page-link")), class: "page-item")
       end
     end
 
-    def previous_or_next_page(page, text, classname)
+    def previous_or_next_page(page, text)
       link_options = @options[:link_options] || {}
 
       if page
-        tag("li", link(text, page, link_options), class: classname)
+        tag("li", link(text, page, link_options.merge(class: "page-link")), class: "page-item")
       else
-        tag("li", tag("span", text), class: "%s disabled" % classname)
+        tag("li", tag("span", text, class: "page-link"), class: "%s disabled" % "page-item")
       end
     end
 
     def gap
-      tag("li", tag("span", ELLIPSIS), class: "disabled")
+      tag("li", tag("span", ELLIPSIS, class: "page-link"), class: "disabled page-item")
     end
 
     def previous_page
       num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page(num, @options[:previous_label], "prev")
+      previous_or_next_page(num, tag("span", "&laquo;", "aria-hidden" => "true"))
     end
 
     def next_page
       num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
-      previous_or_next_page(num, @options[:next_label], "next")
+      previous_or_next_page(num, tag("span", "&raquo;", "aria-hidden" => "true"))
     end
 
     def ul_class
-      ["pagination", @options[:class]].compact.join(" ")
+      ["pagination", @options[:class]].compact.uniq.join(" ")
     end
   end
 end
--- lib/bootstrap_pagination/version.rb
+++ lib/bootstrap_pagination/version.rb
@@ -1,3 +1,3 @@
 module BootstrapPagination
-  VERSION = "1.0.1"
+  VERSION = "1.0.1-v4alpha"
 end
